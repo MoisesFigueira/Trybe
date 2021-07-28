3886dec55c08f6820e8ce8ae79188366edd6367c
MoisésFigueira <moisesmichael@gmail.com>
2021-07-28 19:27:38 -0300

    Variable JavaScript

---
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
@@ -0,0 +1,10 @@
+const myName = 'Moises Figueira';
+const birthCity = 'Belo Horizonte';
+let birthYear = 1991;
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+birthYear = 2030;
+console.log (birthYear);
+// birthCity = 'Bellzonte';
+// console.log (birthCity); it is not possible to change the value of a constant
