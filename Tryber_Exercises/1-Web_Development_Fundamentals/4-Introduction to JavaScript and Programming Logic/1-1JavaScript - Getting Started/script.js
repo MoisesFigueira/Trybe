1f808b0780e6b0b5f80040524e0f787d611de8c5
MoisésFigueira <moisesmichael@gmail.com>
2021-07-29 14:54:19 -0300

    exercises-9

--- Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
@@ -181,10 +181,10 @@
 // }
 
 
-// exercises-8
+// exercises-8 & 9
 let num1 = 1;
 let num2 = 3;
-let num3 = 5;
+let num3 = 4;
 if (num1 %2 == 0 || num2 %2 == 0 || num3 %2 == 0) {
 	console.log("true");
 }
