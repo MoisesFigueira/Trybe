4d434a84ba10bd47a1a6e9f1405fa308bc863245
MoisésFigueira <moisesmichael@gmail.com>
2021-07-29 14:53:02 -0300

    exercises-8

--- Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
@@ -155,28 +155,40 @@
 
 
 // exercises-7
-let note = "101";
-if (note < 0 || note > 100){
-		console.log ("invalid number!")
+// let note = "101";
+// if (note < 0 || note > 100){
+// 		console.log ("invalid number!")
+// }
+// else {
+// if (note >= 90) {
+// 	console.log("A");
+// }
+// else if (note >= 80) {
+// 	console.log("B");
+// }
+// else if (note >= 70) {
+// 	console.log("C");
+// }
+// else if (note >= 60) {
+// 	console.log("D");
+// }
+// else if (note >= 50) {
+// 	console.log("E");
+// }
+// else{
+// 	console.log("F");
+// }
+// }
+
+
+// exercises-8
+let num1 = 1;
+let num2 = 3;
+let num3 = 5;
+if (num1 %2 == 0 || num2 %2 == 0 || num3 %2 == 0) {
+	console.log("true");
 }
 else {
-if (note >= 90) {
-	console.log("A");
-}
-else if (note >= 80) {
-	console.log("B");
-}
-else if (note >= 70) {
-	console.log("C");
-}
-else if (note >= 60) {
-	console.log("D");
-}
-else if (note >= 50) {
-	console.log("E");
-}
-else{
-	console.log("F");
-}
+	console.log("false");
 }
 
