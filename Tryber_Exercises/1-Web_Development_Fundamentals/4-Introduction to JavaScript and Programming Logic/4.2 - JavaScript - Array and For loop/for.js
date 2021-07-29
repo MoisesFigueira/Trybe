51c069c69905e6452dc58d47156418aa58468080
MoisésFigueira <moisesmichael@gmail.com>
2021-07-29 20:28:55 -0300

    exercises1

---
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/for.js
@@ -0,0 +1,27 @@
+// // Suponha que temos uma lista com marcas de carros:
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+
+// // Anteriormente para apresentar esses valores, o código que utilizariamos para executar essa tarefa seria:
+// let cars = ['Saab', 'Volvo', 'BMW'];
+// console.log(cars[0]); // Saab
+// console.log(cars[1]); // Volvo
+// console.log(cars[2]); // BMW
+
+
+// // Mas, agora que conhecemos o for , podemos melhorar o código para algo como:
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+// }
+
+// Exercício 1
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index += 1) {
+	console.log(groceryList[index]);
+}
+
+
