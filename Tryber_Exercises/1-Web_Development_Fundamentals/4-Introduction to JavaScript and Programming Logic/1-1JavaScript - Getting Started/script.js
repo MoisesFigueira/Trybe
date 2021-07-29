871558bed60ddb7c9a8b73b917d17607442deaab
MoisésFigueira <moisesmichael@gmail.com>
2021-07-28 21:23:04 -0300

    javascript - primitive types

--- Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/1-1JavaScript - Getting Started/script.js
@@ -1,10 +1,22 @@
-const myName = 'Moises Figueira';
-const birthCity = 'Belo Horizonte';
-let birthYear = 1991;
-console.log (myName);
-console.log (birthCity);
-console.log (birthYear);
-birthYear = 2030;
-console.log (birthYear);
+// const myName = 'Moises Figueira';
+// const birthCity = 'Belo Horizonte';
+// let birthYear = 1991;
+// console.log (myName);
+// console.log (birthCity);
+// console.log (birthYear);
+// birthYear = 2030;
+// console.log (birthYear);
 // birthCity = 'Bellzonte';
 // console.log (birthCity); it is not possible to change the value of a constant
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId)
+console.log(typeof patientAge)
+patientId = '50'
+console.log(typeof patientId)
