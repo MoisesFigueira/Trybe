b80bc6a07e78bd7e8c04ca8a3bea8d095a300fd6
MoisésFigueira <moisesmichael@gmail.com>
2021-07-30 11:50:40 -0300

    exercises3

--- Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/exercises.js
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/exercises.js
@@ -1,17 +1,37 @@
-// Aprofunde seus conhecimentos
-// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
-// Copiar
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// // Aprofunde seus conhecimentos
+// // Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+// // Copiar
+// // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// // 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let index of numbers) {
-	console.log(index)
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index of numbers) {
+// 	console.log(index)
+// }
 
 // 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// for (let index = 0 ; index < numbers.length; index += 1) {
+// 	sum += numbers[index];
+// }
+// console.log(sum)
+
+
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 //  . A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let mean = 0;
+for (let index = 0 ; index < numbers.length; index += 1) {
+	sum += numbers[index];
+}
+mean = sum / numbers.length
+console.log(mean)
+
+
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 // 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 // 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
