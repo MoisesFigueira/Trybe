bd7c42382ef62faa84b298ad36f6e23d15ad3a14
MoisésFigueira <moisesmichael@gmail.com>
2021-07-30 12:47:55 -0300

    exercises8

--- Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/exercises.js
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/exercises.js
@@ -34,23 +34,44 @@
 
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-let mean = 0;
-for (let index = 0 ; index < numbers.length; index += 1) {
-	sum += numbers[index];
-}
-mean = sum / numbers.length;
-if (mean > 20) {
-	console.log("value greater than 20")
-}
-else{
-	console.log("value less than or equal to 20")
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// let mean = 0;
+// for (let index = 0 ; index < numbers.length; index += 1) {
+// 	sum += numbers[index];
+// }
+// mean = sum / numbers.length;
+// if (mean > 20) {
+// 	console.log("value greater than 20")
+// }
+// else{
+// 	console.log("value less than or equal to 20")
+// }
+
 
 // 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let larger = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+
+// }
+
+
 // 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+
 // 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+
 // 8. Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let num =[];
+for (let index = 1; index <= 25; index += 1){
+	num.push(index);
+}
+console.log(num)
+
+
 // 9 .Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
