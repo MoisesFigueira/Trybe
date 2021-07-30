8ca70b638adb72094f2102f44f4ceffc11d4ad32
MoisésFigueira <moisesmichael@gmail.com>
2021-07-30 15:38:06 -0300

    update

---
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.3 - JavaScript - Programming Logic and Algorithms/exercises.js
@@ -0,0 +1,68 @@
+// // O enunciado diz:
+// // Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
+// // Adicionar o array;
+// let fruits = [3, 4, 10, 1, 12];
+
+// // Criar uma variável com valor 0;
+// let fruits = [3, 4, 10, 1, 12];
+// let sum = 0;
+
+// // Criar um loop que percorre o array;
+// let fruits = [3, 4, 10, 1, 12];
+// let sum = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+
+// }
+
+// // Incrementar a variável com o valor correspondente a cada loop;
+// let fruits = [3, 4, 10, 1, 12];
+// let sum = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+//   sum += fruits[index];
+// }
+
+// // Criar um if com a condição da váriavel ser maior que 15;
+// let fruits = [3, 4, 10, 1, 12];
+// let sum = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+//   sum += fruits[index];
+// }
+
+// if (sum > 15) {
+
+// } else {
+
+// }
+
+// // Caso a varíavel obedeça a condição: Imprimir a varíavel;
+// let fruits = [3, 4, 10, 1, 12];
+// let sum = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+//   sum += fruits[index];
+// }
+
+// if(sum > 15) {
+//   console.log(sum);
+// } else {
+
+// }
+
+// Caso não obedeça a condição: Imprimir a mensagem “valor menor que 16”;
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+if (sum > 15) {
+  console.log(sum);
+} else {
+  console.log('valor menor que 16');
+}
+
+// 
