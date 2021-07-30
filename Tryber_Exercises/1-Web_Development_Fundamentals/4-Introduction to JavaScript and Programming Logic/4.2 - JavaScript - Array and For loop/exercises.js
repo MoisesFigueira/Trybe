f60561d331ebabf7dc674594804cf4b07978ef3f
MoisésFigueira <moisesmichael@gmail.com>
2021-07-30 11:57:08 -0300

    exercises4

--- Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/exercises.js
+++ Tryber_Exercises/1-Web_Development_Fundamentals/4-Introduction to JavaScript and Programming Logic/4.2 - JavaScript - Array and For loop/exercises.js
@@ -11,12 +11,12 @@
 
 // 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-for (let index = 0 ; index < numbers.length; index += 1) {
-	sum += numbers[index];
-}
-console.log(sum)
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// for (let index = 0 ; index < numbers.length; index += 1) {
+// 	sum += numbers[index];
+// }
+// console.log(sum)
 
 
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
@@ -33,6 +33,21 @@ console.log(sum)
 
 
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let mean = 0;
+for (let index = 0 ; index < numbers.length; index += 1) {
+	sum += numbers[index];
+}
+mean = sum / numbers.length;
+if (mean > 20) {
+	console.log("value greater than 20")
+}
+else{
+	console.log("value less than or equal to 20")
+}
+
 // 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 // 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 // 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o;
